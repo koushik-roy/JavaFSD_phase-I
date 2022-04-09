9338f6ab39b76a39d880272d0ec35dcff1b04e41
Koushik Roy <jc.koushikroy@gmail.com>
2022-04-09 20:31:32 +0530

    created class for retrieiving files

---
+++ RetrieveFiles.java
@@ -0,0 +1,49 @@
+package phase_I_project;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class RetrieveFiles {
+	
+	public static void run() {
+		System.out.println("Total number of files: "+count());
+		retrieve();
+	}
+
+	
+	private static void retrieve() {
+		try {
+			File folder=new File("D:\\eclipse-workspace\\JavaFSD_phase1\\TestFiles");
+					
+			File[] files=folder.listFiles();
+			for(File file:files) {
+					System.out.println(file.getName());
+				
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static int count() {
+		int count=0;
+		try {
+			File folder=new File("D:\\eclipse-workspace\\JavaFSD_phase1\\TestFiles");
+					
+			File[] files=folder.listFiles();
+			for(File file:files) {
+					count++;
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return (count);
+		
+	}
+	
+	
+}
