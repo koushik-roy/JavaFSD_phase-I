b14d91fcaa3bbf8fc7813e2e921c8af231d08e9c
Koushik Roy <jc.koushikroy@gmail.com>
2022-04-09 20:35:33 +0530

    created class for deleting files and folders

---
+++ DeleteFolder.java
@@ -0,0 +1,29 @@
+package phase_I_project;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class DeleteFolder {
+
+	public static void run() {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the name of the folder:");
+		Scanner sc=new Scanner(System.in);
+		String name=sc.nextLine();
+		if(name.length()==0)
+			System.out.println("please enter the name");
+		else
+			deleteFile(name);
+
+	}
+	
+	private static void deleteFile(String s) {
+		 if(new File("D:\\eclipse-workspace\\JavaFSD_phase1\\TestFiles\\"+s).delete()) {
+			 System.out.println("dolder deleted successfully");
+		 }
+		 else
+			 System.out.println("error: file not found");
+	}
+
+}
+
